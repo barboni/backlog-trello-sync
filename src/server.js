0595af5d2bf775fb5e97b6645eb28258146a98b8
Vlad Barboni <vlad.barboni@gmail.com>
2016-08-16 03:16:50 +0200

    Add 'create-list' route and refactor to avoid duplicate code.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import conf from './config.js'
 import bodyParser from 'body-parser'
 import { login, cb } from './oauth.js'
-import { getUserBoards, createBoard } from './trello.js'
+import { getUserBoards, createBoard, createList } from './trello.js'
 
 const app = express()
 
@@ -16,23 +16,23 @@ if (conf.get('env') === 'development') {
   app.get('/cb', cb)
 }
 
-app.post('/list', (req, res) => {
+app.post('/list-boards', (req, res) => {
   const { body: { token, secret } } = req
   getUserBoards(token, secret)
-    .then(boards => {
-      return boards.map(b => b.name)
-    })
-    .then(names => {
-      res.json(names)
-    })
+    .then(boards => boards.map(b => b.name))
+    .then(names => res.json(names))
 })
 
-app.post('/create', (req, res) => {
+app.post('/create-board', (req, res) => {
   const { body: { token, secret, name } } = req
   createBoard(token, secret, name)
-    .then(result => {
-      res.json(result)
-    })
+    .then(result => res.json(result))
+})
+
+app.post('/create-list', (req, res) => {
+  const { body: { token, secret, name, boardId } } = req
+  createList(token, secret, name, boardId)
+    .then(result => res.json(result))
 })
 
 app.listen(port, () => {
--- src/trello.js
+++ src/trello.js
@@ -1,26 +1,39 @@
 import oauth from './oauth.js'
 
-export const getUserBoards = (token, secret) => {
+const apiBase = 'https://api.trello.com/1'
+
+function request(type, path, token, secret, data) => {
+
+  if (['get', 'post', 'put', 'delete'].indexOf(type) < 0) {
+    throw new Error('request type not supported')
+  }
+
+  const method = oauth[type]
 
   return new Promise((resolve, reject) => {
-    oauth.get('https://api.trello.com/1/members/me/boards', token, secret, (e, data) => {
+    function resolveReject(e, data) => {
       if (e) {
         reject(e)
       }
-
       resolve(JSON.parse(data))
-    })
+    }
+
+    if (data) {
+      method(`${apiBase}/${path}`, token, secret, data, resolveReject)
+    } else {
+      method(`${apiBase}/${path}`, token, secret, resolveReject)
+    }
   })
 }
 
-export const createBoard = (token, secret, name) => {
-  return new Promise((resolve, reject) => {
-    oauth.post('https://api.trello.com/1/boards', token, secret, { name }, (e, data) => {
-      if (e) {
-        reject(e)
-      }
+export const getUserBoards = (token, secret) => {
+  return request('get', 'members/me/boards', token, secret)
+}
 
-      resolve(JSON.parse(data))
-    })
-  })
+export const createBoard = (token, secret, name) => {
+  return request('post', 'boards', token, secret, { name })
+}
+
+export const createList = (token, secret, name, boardId) => {
+  return request('lists', token, secret, { name, idBoard: boardId })
 }
