5c6ee80a2ddda324aa562d870a002c87f5f56385
Vlad Barboni <vlad.barboni@gmail.com>
2016-08-16 02:09:31 +0200

    Add oauth mechanism for getting a token and a secret and wrapping requests with them.

---
+++ src/oauth.js
@@ -0,0 +1,45 @@
+import { OAuth } from 'oauth'
+import conf from './config.js'
+import url from 'url'
+
+const requestURL = "https://trello.com/1/OAuthGetRequestToken"
+const accessURL = "https://trello.com/1/OAuthGetAccessToken"
+const authorizeURL = "https://trello.com/1/OAuthAuthorizeToken"
+const appName = 'Backlog sync'
+
+const ip = conf.get('ip')
+const port = conf.get('port')
+
+const loginCallback = `http://${ip}:${port}/cb`
+
+const key = conf.get('key')
+const secret = conf.get('secret')
+
+const oauth = new OAuth(requestURL, accessURL, key, secret, '1.0', loginCallback, 'HMAC-SHA1')
+
+const oauthSecrets = {}
+
+export const login = (req, res) => {
+  oauth.getOAuthRequestToken((error, token, tokenSecret) => {
+    oauthSecrets[token] = tokenSecret
+    res.writeHead(302, { 'Location': `${authorizeURL}?oauth_token=${token}&name=${appName}` })
+    res.end()
+  })
+}
+
+export const cb = (req, res) => {
+  const query = url.parse(req.url, true).query
+
+  const token = query.oauth_token
+  const tokenSecret = oauthSecrets[token]
+  const verifier = query.oauth_verifier
+
+  oauth.getOAuthAccessToken(token, tokenSecret, verifier, (error, accessToken, accessTokenSecret) => {
+    res.json({
+      accessToken,
+      accessTokenSecret
+    })
+  })
+}
+
+export default oauth
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import conf from './config.js'
 import bodyParser from 'body-parser'
+import { login, cb } from './oauth.js'
 
 const app = express()
 
@@ -8,6 +9,11 @@ const port = conf.get('port')
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+if (conf.get('env') === 'development') {
+  app.get('/login', login)
+  app.get('/cb', cb)
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`)
